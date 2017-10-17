0b3ae1603e73ac3de1b56147e36cc77f4e8531c3
David Janas <davidjanasr@gmail.com>
2017-10-17 14:45:35 -0400

    Build first URL path for JSON POST requests

---
+++ leaguetool/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LeaguetoolConfig(AppConfig):
+    name = 'leaguetool'
---
+++ leaguetool/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+
+# Create your views here.
+@csrf_exempt
+def index(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        return JsonResponse(data)
