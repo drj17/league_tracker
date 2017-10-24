85f7f3b9cea9e97e1bdf54132be2dd5428499030
David Janas <davidjanasr@gmail.com>
2017-10-24 16:55:49 -0400

    use gzip for decoding string

--- leaguetool/views.py
+++ leaguetool/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
+import zlib
+
 
 
 # Create your views here.
@@ -10,6 +12,8 @@ def index(request):
     if request.method == 'POST':
         print(request.body)
         print(type(request.body))
+        decompressed_data=zlib.decompress(request.body, 16+zlib.MAX_WBITS)
+        print(decompressed_data)
         ustr_to_load = request.body.decode("ISO-8859-1")
         print(ustr_to_load)
         data = json.loads(ustr_to_load)
