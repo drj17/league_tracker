c50d278fdf68ff4f06b017af4ade8c8b727c6046
David Janas <davidjanasr@gmail.com>
2017-10-24 16:32:16 -0400

    change unicode to str for python 3

--- leaguetool/views.py
+++ leaguetool/views.py
@@ -8,6 +8,6 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
-        ustr_to_load = unicode(request.body, "ISO-8859-1")
+        ustr_to_load = str(request.body, "ISO-8859-1")
         data = json.loads(ustr_to_load)
         return JsonResponse(data)
